4d163ec6ee66730dcd1aa7561f01f307a386d502
unknown07724 <trash1.junk99@gmail.com>
2024-07-16 16:30:57 -0500

    Update davinci.js

--- modules/davinci.js
+++ modules/davinci.js
@@ -6,3 +6,21 @@ ctx.beginPath();
 ctx.arc(x, y, r, 0, 2 * Math.PI);
 ctx.stroke();
 }
+
+function draw_colored_circle(x,y,r,fill_color,line_width,stroke_style) {
+  ctx.beginPath();
+ctx.arc(x, y, r, 0, 2 * Math.PI);
+ctx.fillStyle = "fill_color";
+ctx.fill();
+ctx.lineWidth = line_width;
+ctx.strokeStyle = stroke_style;
+ctx.stroke();
+}
+
+function draw_half_circle(x,y,r,color) {
+  ctx.beginPath();
+ctx.arc(x, y, r, 0, Math.PI);
+ctx.fillStyle = "color";
+ctx.fill();
+ctx.stroke();
+}
