df25957de2f6952e45ff487af148cdda96d472e7
unknown07724 <trash1.junk99@gmail.com>
2024-07-16 16:18:14 -0500

    Create atom.js

---
+++ modules/atom.js
@@ -0,0 +1,12 @@
+// by unknown07724
+// math module to use for projects
+function rad_to_degr(radians)
+{
+  // Multiply radians by 180 divided by Pi to convert to degrees.
+  return radians * (180/Math.PI);
+}
+function half(number)
+{
+  // Multiply radians by 180 divided by pi to convert to degrees.
+  return number / 2
+}
