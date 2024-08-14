9dae24be55a97c6cdddcf583218d16c2c62545f9
unknown07724 <trash1.junk99@gmail.com>
2024-08-14 06:24:52 -0500

    Update utils.js

--- projects/gine/utils.js
+++ projects/gine/utils.js
@@ -38,3 +38,34 @@ function drawHalfCircle(context, x, y, radius, color, isTop = true) {
 
 // Export functions if using modules
 // export { drawCircle, drawHalfCircle };
+
+/**
+ * Draws a hexagon on the canvas.
+ * @param {CanvasRenderingContext2D} context - The 2D drawing context of the canvas.
+ * @param {number} x - The x coordinate of the center of the hexagon.
+ * @param {number} y - The y coordinate of the center of the hexagon.
+ * @param {number} radius - The radius of the hexagon (distance from the center to a vertex).
+ * @param {string} color - The fill color of the hexagon.
+ */
+function drawHexagon(context, x, y, radius, color) {
+    const hexagonPoints = [];
+
+    for (let i = 0; i < 6; i++) {
+        const angle = (Math.PI / 3) * i; // 60 degrees in radians
+        const px = x + radius * Math.cos(angle);
+        const py = y + radius * Math.sin(angle);
+        hexagonPoints.push({ x: px, y: py });
+    }
+
+    context.beginPath();
+    context.moveTo(hexagonPoints[0].x, hexagonPoints[0].y);
+
+    for (let i = 1; i < hexagonPoints.length; i++) {
+        context.lineTo(hexagonPoints[i].x, hexagonPoints[i].y);
+    }
+
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+}
+
