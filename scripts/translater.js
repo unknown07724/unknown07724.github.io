3927d09de8d9b4f29849622c96ed9585779a962a
unknown07724 <trash1.junk99@gmail.com>
2024-11-20 15:45:19 -0600

    Update translater.js

--- scripts/translater.js
+++ scripts/translater.js
@@ -1,1 +1,20 @@
-
+  if (navigator.langauge == "de-DE") {
+   window.location.replace("/de/");
+} else {
+ 
+}
+ if (navigator.langauge == "fr-FR") {
+   window.location.replace("/fr/");
+} else {
+ 
+}    
+if (navigator.langauge == "it-IT") {
+   window.location.replace("/it/");
+} else {
+ 
+}
+if (navigator.langauge == "pl-PL") {
+   window.location.replace("/pl/");
+} else {
+ 
+}
