a02c248c6c59311aa05e7d4c26123224c0c70592
unknown07724 <trash1.junk99@gmail.com>
2025-02-12 11:51:26 -0600

    Update sub.js

--- projects/postbox/sub.js
+++ projects/postbox/sub.js
@@ -65,7 +65,7 @@ async function loadSubredditPosts(subredditId) {
 
             postElement.innerHTML = `
                 <div class="post-meta">
-                    <span>Posted by <a href="user.html#${post.username}">${post.username}</a></span>
+                    <span>Posted by <a href="user.html#${post.user}">${post.username}</a></span>
                     <p>Posted on ${post.posted}</p>
                 </div>
                 <h4>${post.title}</h4>
