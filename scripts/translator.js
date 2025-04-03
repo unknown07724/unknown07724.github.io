f78cff56c16c7d5eb5cff5f7a85c2a84fe026bc5
unknown07724 <trash1.junk99@gmail.com>
2025-04-02 21:40:12 -0500

    Update translator.js

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,26 +1,27 @@
-  if (navigator.langauge == "de-DE") {
-   window.location.replace("/de/");
-} else {
- 
-}
- if (navigator.langauge == "fr-FR") {
-   window.location.replace("/fr/");
-} else {
- 
-}    
-if (navigator.langauge == "it-IT") {
-   window.location.replace("/it/");
-} else {
- 
-}
-if (navigator.langauge == "pl-PL") {
-   window.location.replace("/pl/");
-} else {
- 
-}
-
-if (navigator.langauge == "uk-UA") {
-   window.location.replace("/uk/");
-} else {
- 
+switch (navigator.language) {
+  case "de-DE":
+    window.location.replace("/de/");
+    break;
+  case "fr-FR":
+    window.location.replace("/fr/");
+    break;
+  case "it-IT":
+    window.location.replace("/it/");
+    break;
+  case "pl-PL":
+    window.location.replace("/pl/");
+    break;
+  case "uk-UA":
+    window.location.replace("/uk/");
+    break;
+  case "da-DK":
+    window.location.replace("/da/");
+    break;
+  case "cs-CZ":
+    window.location.replace("/cz/");
+    break;
+  case "en-US":
+    break;
+  default:
+    break;
 }
