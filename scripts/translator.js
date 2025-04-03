00aacfd0786eb667280de24886be8b23e1545cdb
unknown07724 <trash1.junk99@gmail.com>
2025-04-02 21:53:57 -0500

    Update translator.js

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,24 +1,24 @@
 switch (navigator.language) {
   case "de-DE":
-    window.location.replace("/de/");
+    window.location.replace("/langs/de/");
     break;
   case "fr-FR":
-    window.location.replace("/fr/");
+    window.location.replace("/langs/fr/");
     break;
   case "it-IT":
-    window.location.replace("/it/");
+    window.location.replace("/langs/it/");
     break;
   case "pl-PL":
-    window.location.replace("/pl/");
+    window.location.replace("/langs/pl/");
     break;
   case "uk-UA":
-    window.location.replace("/uk/");
+    window.location.replace("/langs/uk/");
     break;
   case "da-DK":
-    window.location.replace("/da/");
+    window.location.replace("/langs/da/");
     break;
   case "cs-CZ":
-    window.location.replace("/cz/");
+    window.location.replace("/langs/cz/");
     break;
   case "en-US":
     break;
